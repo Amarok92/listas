945adb0f7ee72b66cf705198cdc24188cf4339df
andres <andrestovar1488@gmail.com>
2022-06-22 17:45:46 -0500

    listas python

---
+++ README.md
@@ -0,0 +1,5 @@
+# Python-3-playlist
+Ejercicios de python para renderizar carpetas de mi ordenador 
+y evitarles la caja de pandora de sus archivos en su pc /../
+
+
---
+++ programas/ordenaFicheros.py
@@ -0,0 +1,36 @@
+print ('Hola Mundo Desde Python!')
+home = "./../../Escritorio/Test"
+# Leer la extensión de un fichero
+import os
+import shutil
+for file in os.listdir(home):
+    if file.endswith(""):
+        x = (file.split("."))
+        if(len(x) > 1):
+            # tenemos la extensión
+            extension = (x[-1])#con -1 elegiremos siempre el último elemento de array de extensiones.
+            nombreFichero = os.path.join("", file)
+            print(nombreFichero)
+            print(extension)
+            #Comprobar si existe un directorio con la extensión que estamos iterando
+            existeDir = os.path.isdir(home+"/"+extension)
+            print(home+"/"+extension)
+            if(existeDir):
+                print("Existe!")
+                # Mueve el fichero a esta ruta
+                shutil.move(((home)+"/"+nombreFichero),((home)+"/"+extension + "/" + nombreFichero))
+                print("Fichero" + nombreFichero + " movido a " + ((home)+"/"+extension))
+            else:
+                print("NO Existe!")
+                # Crea el directorio en la carpeta home con el nombre de la extensión
+                os.mkdir((home)+"/"+extension)
+                print("Nueva carpeta "+ extension + " creada")
+                # Mueve el fichero a esta ruta
+                shutil.move(((home)+"/"+nombreFichero),((home)+"/"+extension+ "/" + nombreFichero))
+                print("Fichero" + nombreFichero + " movido a " + ((home)+"/"+extension))
+
+            
+            
+
+
+        
